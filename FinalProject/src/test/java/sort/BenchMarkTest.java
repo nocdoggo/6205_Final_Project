41fb830b5a902e2d0f1009a42299fa55e7553cf9
FeifanChenXi <90587617+FeifanChenXi@users.noreply.github.com>
2021-12-06 11:02:16 +0800

    test

--- FinalProject/src/test/java/sort/BenchMarkTest.java
+++ FinalProject/src/test/java/sort/BenchMarkTest.java
@@ -9,8 +9,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class BenchMarkTest {
     public static String[] toText(File file){
@@ -80,6 +83,77 @@ public class BenchMarkTest {
 
         double meanTime=BenchMark.toMillisecs(timeGap)/5;
         System.out.println("HuskySortBenchMark-----------length is"+length+"---------Mean Time is----------"+meanTime+"ms");
+
+    }
+    public static void MultiThreadBenchMark(int length) throws ExecutionException, InterruptedException {
+
+        int loops=5;
+        long start=  BenchMark.StartTime();
+        for(int l=0;l<loops;l++){
+            int arrSize = length;
+            String[] unsorted = new String[arrSize];
+
+            Random randomizer = new Random();
+
+            for ( int i = 0; i < arrSize; i++ ) {
+                unsorted[i] = Integer.toString(randomizer.nextInt( 10_00000 ));
+            }
+            ///////////////////////////////////////
+
+            List<Future> futureList = new ArrayList<>();
+            // Get the maximum number of threads available for the given processor
+            int max_thread_count = Runtime.getRuntime().availableProcessors();
+            System.out.println("The maximum physical threads allowed on this processor is " + max_thread_count);
+            int perBlockSize = arrSize/max_thread_count;
+            long startTime = System.currentTimeMillis();
+
+            // Launch the pool
+            final ExecutorService poolingApp = Executors.newFixedThreadPool(max_thread_count);
+            ArrayList<PoolMerge> mergers = new ArrayList<>();
+            for (int i = 0; i < max_thread_count; i++) {
+
+
+                String[] partialArr = new String[perBlockSize];
+
+
+                System.arraycopy( unsorted, i*perBlockSize, partialArr, 0, perBlockSize );
+                PoolMerge PoolMerge = new PoolMerge(partialArr);
+
+                futureList.add(poolingApp.submit(PoolMerge));
+                //add PoolMerge to list to get result in future
+                mergers.add(PoolMerge);
+            }
+            for (Future<Double> future : futureList) {
+                future.get();
+            }
+            poolingApp.shutdown();
+            int j = 0;
+            // array to get result
+            String[] mergered = new String[arrSize];
+            // sequential merge of all partialArr of array
+            for (PoolMerge PoolMerge:mergers){
+                if (j == 0) {
+                    mergered = PoolMerge.getSorted();
+                    j+=1;
+                }
+                else{
+                    String[] partialArr = PoolMerge.getSorted();
+                    mergered = MergeProcess.merge( mergered, partialArr);
+                }
+            }
+            long overallRuntime = System.currentTimeMillis() - startTime;
+            System.out.println("With " + max_thread_count + " of threads, it needs " + overallRuntime + " milliseconds");
+            startTime = System.currentTimeMillis();
+            Arrays.sort(unsorted);
+            overallRuntime = System.currentTimeMillis() - startTime;
+            System.out.println("With single thread, it takes " + overallRuntime + " milliseconds");
+        }
+
+        long end=BenchMark.EndTime();
+        long timeGap=end-start;
+
+        double meanTime=BenchMark.toMillisecs(timeGap)/5;
+        System.out.println("MultiThreadBenchMark-----------length is"+length+"---------Mean Time is----------"+meanTime+"ms");
 
     }
     public static void TimSortBenchMark(int length){
@@ -163,8 +237,8 @@ public class BenchMarkTest {
         System.out.println("SqlSortBenchMarkByStroke-----------length is"+length+"---------Mean Time is----------"+meanTime+"ms");
 
     }
-    public static void main(String[] args) throws SQLException {
-        int length=25000;
+    public static void main(String[] args) throws SQLException, ExecutionException, InterruptedException {
+     /*   int length=25000;
         for(int i=0;i<6;i++) {
             DualPrivotSortBenchMark(length);
             length*=2;
@@ -192,6 +266,12 @@ public class BenchMarkTest {
         length=25000;
         for(int i=0;i<6;i++) {
             SqlSortBenchMarkByStroke(length);
+            length*=2;
+        }
+        length=25000;*/
+        int length=25000;
+        for(int i=0;i<6;i++) {
+            MultiThreadBenchMark(length);
             length*=2;
         }
 
